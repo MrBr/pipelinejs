7558fa0075f666a63cb095e58d26a4c16332dd87
MrBr <lbracano@gmail.com>
2017-05-24 00:23:52 +0200

    Improve PipeDescriptor transformers
    - Transformers now have shorthands

--- src/PipeDescriptor.js
+++ src/PipeDescriptor.js
@@ -1,6 +1,16 @@
 import _ from 'lodash';
+import { pick } from './transformers/in';
+import { append } from './transformers/out';
 
 const defaultMeta = { connected: true };
+
+function resolveInTransformer(transformer) {
+  return _.isString(transformer) ? pick(transformer) : transformer;
+}
+
+function resolveOutTransformer(transformer) {
+  return _.isString(transformer) ? append(transformer) : transformer;
+}
 
 export default class PipeDescriptor {
   constructor() {
@@ -18,8 +28,27 @@ export default class PipeDescriptor {
     return this;
   }
 
-  create(type, pipe, inTransformer, outTransformer, errTransformer, extra = {}) {
+  /**
+   * Create Pipe descriptor.
+   * Process arguments optional values to required type.
+   *
+   * @param type {string}
+   * @param pipe {function | Pipeline}
+   * @param inTransformer {string | function}
+   *  A string is converted to the pick transformer, a function is left as is.
+   * @param outTransformerArg {string | function(result, stream)}
+   *  A string is converted to the append transformer, a function is left as is.
+   * @param errTransformerArg {string | function}
+   *  Same as the outTransformer.
+   * @param extra {object}
+   * @returns {PipeDescriptor}
+   */
+  create(type, pipe, inTransformerArg, outTransformerArg, errTransformerArg, extra = {}) {
     const meta = { ...extra, ...defaultMeta}; // Additional info
+
+    const inTransformer = resolveInTransformer(inTransformerArg);
+    const outTransformer = resolveOutTransformer(outTransformerArg);
+    const errTransformer = resolveOutTransformer(errTransformerArg);
 
     return this.setup({
       type,
