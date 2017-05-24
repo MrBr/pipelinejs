76fd3c7c6fbc930a20421ef4fe8c88cbc60dd6f0
MrBr <lbracano@gmail.com>
2017-05-24 23:53:29 +0200

    Add more pipes and transformers

--- src/pipes/index.js
+++ src/pipes/index.js
@@ -12,15 +12,39 @@ export const requireProp =
       noop(!_.has(stream, prop) && close(`Missing required prop ${prop}`));
 
 export const log =
-  (message, prop) =>
+  (prop, message = 'Log:') =>
     stream =>
       console.log(message, _.get(stream, prop));
 
 export const logStream =
+  (message = 'Stream:') =>
     stream =>
-      console.log(stream);
+      console.log(message, stream);
 
-export const promise =
-  p =>
+export const proxy =
+  result =>
     stream =>
-      p;
+      result;
+
+export const stringify =
+  (source, destination) =>
+    stream =>
+      _.set(stream, destination || source, JSON.stringify(_.get(stream, source)));
+
+export const stringifyStream =
+  stream =>
+    JSON.stringify(stream);
+
+export const invoke =
+  (method, objProp, prepareArgs = proxy) =>
+    stream =>
+      _.get(stream, objProp)[method](prepareArgs(stream));
+
+export const newStream =
+  stream =>
+    ({});
+
+export const append =
+  (prop, value) =>
+    stream =>
+      _.set(stream, prop, value);
---
+++ src/transformers/index.js
@@ -0,0 +1,5 @@
+// Shared transformers
+
+export const noArgs =
+  stream =>
+    undefined;
