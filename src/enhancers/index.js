4bf1aa36b32deaacd38a38bac1c30defb5425f96
MrBr <lbracano@gmail.com>
2017-05-25 22:31:25 +0200

    Improve enhancers
    - Add when

--- src/enhancers/index.js
+++ src/enhancers/index.js
@@ -5,13 +5,13 @@ import { noop } from '../helpers';
 export const transformIn =
   transformer =>
     pipeline =>
-      (stream, close) =>
-        pipeline(transformer(stream), close);
+      stream =>
+        pipe(transformer(stream), pipeline);
 
 export const transformOut =
   transformer =>
     pipeline =>
-      (stream, close) =>
+      stream =>
         pipe(stream, pipeline).then(transformer);
 
 export const transformError =
@@ -26,7 +26,7 @@ export const transformError =
 export const disconnect =
   pipeline =>
     stream =>
-      noop(pipeline({ ...stream }, () => {}));
+      noop(pipe({ ...stream }, pipeline));
 
 /**
  * Change given stream prop to the transformer return value.
@@ -38,3 +38,16 @@ export const transformProp =
     pipeline =>
       (stream, close) =>
         pipeline(_.set(stream, prop, transformer(_.get(stream, prop))), close);
+
+/**
+ * Call pipeline only when case is fulfilled.
+ * @param comparator {string | array | function} Comparator function or prop path (lodash path)
+ * @param value {*}
+ */
+export const when = // TODO - find a better name (switch)
+  (comparator, value) =>
+    pipeline =>
+      stream =>
+        _.isFunction(comparator)
+          ? comparator(stream) : _.get(stream, comparator) === value
+            ? pipe(stream, pipeline) : noop();
